32ef9e2a55e3f5699a5bc5368eb3c1cd21849305
Julie Nginn <nginn.julie@gmail.com>
2019-07-24 12:09:46 +0200

    Sidebar + routes

---
+++ src/components/sidebar/fixtures.js
@@ -0,0 +1,36 @@
+export const activities = [
+  {
+    name: 'eat',
+    icon: 'eat.png',
+    path: 'kitchen'
+  },
+  {
+    name: 'toilet',
+    icon: 'toilet.png',
+    path: 'toilet'
+  },
+  {
+    name: 'bath',
+    icon: 'bath.png',
+    path: 'bathroom'
+  },
+  {
+    name: 'sleep',
+    icon: 'sleep.png'
+  },
+  {
+    name: 'wakeup',
+    icon: 'wakeup.png'
+  },
+  {
+    name: 'play',
+    icon: 'play.png',
+    path: 'garden'
+  }
+  ,
+  {
+    name: 'dance',
+    icon: 'dance.png',
+    path: 'dancefloor'
+  }
+]
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import map from 'lodash/map'
+import { activities } from './fixtures'
+import { getPath } from '../../routes'
+import { Link } from 'react-router-dom'
+
+const StyledSidebar = styled.div`
+  padding: 10px 3px;
+  border: 1px solid black;
+  border-radius: 30px;
+  height: 400px;
+  width: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const StyledButton = styled.div`
+  margin: 10px 5px;
+  cursor: pointer;
+  img {
+    height: 30px;
+  }
+`
+
+const Sidebar = () => {
+  return (
+    <StyledSidebar>
+      {map(activities, activity =>
+      <Link to={activity.path && getPath(activity.path)}>
+        <StyledButton>
+          <img src={require(`../../img/icons/${activity.icon}`)} alt={`Icone ${activity.name}`} />
+        </StyledButton>
+      </Link>
+      )}
+    </StyledSidebar>
+  )
+}
+
+export default Sidebar
