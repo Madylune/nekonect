f6a8de38a90568df6a3fd64b57ac5ac409fcf17f
Julie Nginn <nginn.julie@gmail.com>
2019-07-25 10:42:37 +0200

    wip

--- src/components/garden/index.js
+++ src/components/garden/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledGarden = styled.div`
-  background-image: url(${require('../../img/garden.jpg')});
-  height: 100%;
+
 `
 
 const Garden = () => {
