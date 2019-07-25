eda37f2c617dee521c1ea625a9053cfc3136e503
fredbonvalot <bonvalotfrederic@gmail.com>
2019-07-25 09:59:07 +0200

    Modif kitchen + add icons

--- src/components/Kitchen/index.js
+++ src/components/Kitchen/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import map from 'lodash/map'
 
 const Styled = styled.div`
   background: url(${require('../../img/neko_nect_cuisine.jpg')});
@@ -7,53 +8,91 @@ const Styled = styled.div`
   background-position: center;
   text-align: center;
   height: 100vh;
-  padding-top: 80px;
   
 
   .Icon-kitchen {
     background: #ffffff99;
     padding: 15px 20px;
     border-radius: 50px;
-    margin-left: 20px;
+    margin-left: 25%;
     margin-right: 20px;
-    display: flex;
-    justify-content: space-around;
+    width: 60%;
+    position: absolute;
+    top: 7%;
     
     img {
-      width: 53px;
-      height: 50px;
+      width: 49px;
+      height: 42px;
     }
     
+    img.Icon-burger, img.Icon-apple, img.Icon-coffee, img.Icon-water {
+      margin-right: 20px;
+    }
+
+    img.Icon-burger, img.Icon-apple, img.Icon-cake {
+      margin-bottom: 10px;
+    }
   }
 
-  .active {
-    background: #80808070;
-    padding: 8px;
-    border-radius: 50%;
-  }
 
   img.Gif-faim {
-      width: 180px;
       position: absolute;
-      bottom: 116px;
-      right: 84px;
+      width: 180px;
+      bottom: 3%;
+      right: 6%;
     }
 
 `
 
+const foods = [
+  {
+    name: "burger",
+    icon: "burger.png",
+  },
+  {
+    name: "apple",
+    icon: "apple.png",
+  },
+  {
+    name: "cake",
+    icon: "cake.png",
+  },
+  {
+    name: "coffee",
+    icon: "coffee.png",
+  },
+  {
+    name: "water",
+    icon: "water.png",
+  },
+  {
+    name: "pint",
+    icon: "pint.png",
+  },
+]
+
+
 class Kitchen extends Component {   
-  
+    
+    eat = value => {
+
+                  
+      document.getElementById(foods[value].name).classList.add('animate');
+      document.getElementsByClassName('animate').classList.remove('animate');
+
+      console.log(foods[value])
+    }
+
     render() {
       
       return (
         
         <Styled>
             
-
             <div className="Icon-kitchen">
-                <img src={require('../../img/icon/burger.png')} className="Icon-burger" alt="Burger" />
-                <img src={require('../../img/icon/apple.png')} className="Icon-apple active" alt="Apple" />
-                <img src={require('../../img/icon/cake.png')} className="Icon-cake" alt="Cake" />
+              {map(foods, (food, i) => 
+                <img src={require(`../../img/icons/${food.icon}`)} id={food.name} className={`Icon-${food.name}`} alt={`${food.name}`} onClick={() => this.eat(i)} />
+              )}                  
             </div>
             
             <img src={require('../../img/gif/faim.gif')} className="Gif-faim" alt="Pusheen qui a faim" />
