35173f48537a30a3147d5c6bd5f6aa7b688bd83d
Julie Nginn <nginn.julie@gmail.com>
2019-08-22 15:41:50 +0200

    Add Loader when searching Neko

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import get from 'lodash/get'
+import isEmpty from 'lodash/isEmpty'
 import styled from 'styled-components'
 import './index.css'
 import CreateNeko from './components/CreateNeko'
 import GameOver from './components/GameOver'
 import Home from './components/Home'
+import Loader from './components/Loader'
+import { db } from './api/firebase'
+import { NEKO_CREATE_SUCCESS } from './reducers/neko'
 
 const StyledApp = styled.div`
   margin: 0;
@@ -15,20 +19,53 @@ const StyledApp = styled.div`
   overflow: hidden;
 `
 
-const App = ({ isDead, neko }) => (
-  <StyledApp>
-    {!neko ? (
-      <CreateNeko />
-    ) : isDead ? (
-      <GameOver />
-    ) : (
-      <Home />
-    )}
-  </StyledApp>
-)
+class App extends Component {
+  state = {
+    isLoading: false
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true })
+
+    db.collection('neko')
+    .get()
+    .then(querySnapshot => {
+      const data = querySnapshot.docs.map(doc => doc.data())
+      this.props.createNeko({
+        name: get(data, ['0', 'name']),
+        birthdate: get(data, ['0', 'birthdate']),
+        sexe: get(data, ['0', 'sexe'])
+      })
+      this.setState({ isLoading: false })
+    })
+  }
+  
+  render() {
+    const { isLoading } = this.state
+    const { isDead, neko } = this.props
+    return isLoading ? ( 
+      <Loader />
+      ) : (
+      <StyledApp>
+        {isEmpty(neko) ? (
+          <CreateNeko />
+        ) : isDead ? (
+          <GameOver />
+        ) : (
+          <Home />
+        )}
+      </StyledApp>
+    )
+  }
+}
 
 const mapStateToProps = state => ({
-  isDead: get(state, ['mood', 'isDead'])
+  isDead: get(state, ['mood', 'isDead']),
+  neko: get(state, 'neko')
 })
 
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = dispatch => ({
+  createNeko: ({ name, sexe, birthdate }) => dispatch({ type: NEKO_CREATE_SUCCESS, payload: { name, sexe, birthdate } })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
